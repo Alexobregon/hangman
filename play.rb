325a9558ff56ce91dc55ae9c24cbdd6704d087d2
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-27 15:33:08 -0800

    added words and guesses, along with new game puts

--- play.rb
+++ play.rb
@@ -1,13 +1,28 @@
 class Hangmann
 
     def initialize
-    
+        @letters = ('a'..'z').to_a
+        @word = words.sample
+    end
+
+    def word 
+    [
+        ["cricket", "A game played by gentalmen"],
+        ["jogging", "We are not walking"],
+        ["celabrate", "Remembering special moments"],
+        ["continent", "There are 7 pf these"],
+        ["exotic", "Not from around here"],
+    ]
+
+
     end
     
     def begin
         
-    
+        puts "new game started... yourr clue is #{ @word.first }"
+        guess = gets.chomp
         
+        puts " you guessed #{guess}"
     end
 
 end
