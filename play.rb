7e309b6fd2b2cdc3f5ec4b1199d14b704836d719
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-27 15:48:35 -0800

    added good/bad guess check with puts

--- play.rb
+++ play.rb
@@ -10,19 +10,42 @@ class Hangman
         ["cricket", "A game played by gentalmen"],
         ["jogging", "We are not walking"],
         ["celabrate", "Remembering special moments"],
-        ["continent", "There are 7 pf these"],
+        ["continent", "There are 7 of these"],
         ["exotic", "Not from around here"],
     ]
 
 
     end
+
+    def print_teaser
+        word_teaser = ""
+        @word.first.size.times do
+            word_teaser += "_ "
+            end
+    
+            puts word_teaser
+    end
+
+    def make_guess 
+        puts "enter a letter"
+        guess = gets.chomp
+
+       good_guess = @word.first.include? guess
+
+       if good_guess
+        puts "Good Guess!"
+       else
+        puts "Bad Guess!"
+       end
+    end
     
     def begin
         
-        puts "new game started... your clue is #{ @word.first }"
-        guess = gets.chomp
-        
-        puts " you guessed #{guess}"
+        puts "new game started... your clue is #{ @word.first.size } characters long"
+        print_teaser
+
+        puts "Clue: #{ @word.last }"
+        make_guess
     end
 
 end
